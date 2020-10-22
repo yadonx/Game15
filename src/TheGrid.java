7ac6aed98359e5ddd9ee4d0d50aa44376c629034
Liliana Montini Pitra <liliana.pitra@gmail.com>
2020-10-22 14:43:52 +0200

    Created 3 buttons

--- src/TheGrid.java
+++ src/TheGrid.java
@@ -28,6 +28,9 @@ public class TheGrid extends JFrame {
     private JButton button15;
     private JButton button16;
     private JPanel grid2;
+    private JRadioButton musicRadioButton;
+    private JButton iziModeButton;
+    private JButton newGameButton;
 
     private JButton[] buttonArray = new JButton[]{button1, button2, button3, button4, button5, button6,
             button7, button8, button9, button10, button11, button12, button13, button14, button15, button16};
